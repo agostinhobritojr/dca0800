66dcb71a2d6c60cb0babfb366828eeb373703af3
agostinhobritojr <agostinhobritojr@gmail.com>
2017-08-11 12:33:29 -0300

    algoritmos

--- introducao/main.c
+++ introducao/main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
 int main(){
-    int x;
-    int y, z;
+  int x;
+  int y, z;
+  /*  isto eh um comentario
+     * que se extende por varias
+     * li
+     * nhas
 
-    x = 2;
-    y = 3;
-    z = 4;
-    x = y + z;
+      */
+  x = 2;
+  y = 3;
+  z = 4;
+  x = y + z;
 
-    printf("alo=%d x=%d\n", 392, x);
-    return 0;
+  printf("alo=%d x=%d\n", 392, x);
+
+  printf("digite o valor de x: ");
+  scanf("%d", &x);
+
+  printf("alo=%d x=%d\n", 392, x);
+
+
+  return 0;
 }
