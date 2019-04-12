70aaff5c8bee062cecee7c448b0fcbd461bc5b99
agostinhobritojr <agostinhobritojr@gmail.com>
2019-04-12 12:21:22 -0300

    funcao

---
+++ funcao/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+// VIA DE REGRA, VARIAVEIS
+// GLOBAIS DEVEM SER EVITADAS
+// variavel global
+int jose;
+
+int maisum(int a){
+  // variavel local
+  int retorno;
+  retorno = a+1;
+  printf("jose = %d\n", jose);
+  jose = 33;
+  printf("retorno= %d\n", retorno);
+  return retorno;
+}
+
+int mudax(int x){
+  x = 5;
+}
+
+float media(float a, float b){
+  float retorno;
+  retorno = (a+b)/2;
+  return retorno;
+}
+
+int main(){
+  int x, y;
+  float a=4, b=5;
+  x = 3;
+  jose = 45;
+
+  printf("media = %f\n", media(a,b));
+
+  printf("jose = %d\n", jose);
+  y = maisum(x);
+  printf("y = %d\n", y);
+  printf("jose = %d\n", jose);
+
+  mudax(x);
+  printf("x = %d\n", x);
+
+  return 0;
+}
+
+
+
