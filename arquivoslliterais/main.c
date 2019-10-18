a1cf0627d4f24250954526dae292f2e0314c22c9
agostinhobritojr <agostinhobritojr@gmail.com>
2019-10-18 11:52:28 -0300

    projeto

--- arquivoslliterais/main.c
+++ arquivoslliterais/main.c
@@ -3,12 +3,15 @@
 int main(){
   FILE *fin;
   char nome[40];
-  int i;
+
 
   fin = fopen("/home/ambj/literais.txt","r");
-  do{
-    fscanf(fin,"%s", nome);
-    printf("leu: %s\n", nome);
-  }while(feof(fin) == 0);
+  while(feof(fin) == 0){
+//    fscanf(fin,"%s", nome);
+    fgets(nome,40,fin);
+    if(feof(fin) == 0){
+      printf("leu: %s\n", nome);
+    }
+  }
   return 0;
 }
